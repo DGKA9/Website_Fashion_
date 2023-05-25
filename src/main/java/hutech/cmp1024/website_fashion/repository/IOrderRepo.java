1caef5fdfd5d45309f758b8fc69119b27df4a06c
Nvdqb73 <88336997+Nvdqb73@users.noreply.github.com>
2023-05-25 23:45:35 +0700

    Hoàn thành CRUD Category

--- src/main/java/hutech/cmp1024/website_fashion/repository/IOrderRepo.java
+++ src/main/java/hutech/cmp1024/website_fashion/repository/IOrderRepo.java
@@ -3,5 +3,5 @@ package hutech.cmp1024.website_fashion.repository;
 import hutech.cmp1024.website_fashion.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IOrderRepo extends JpaRepository<Order, Long> {
+public interface  IOrderRepo extends JpaRepository<Order, Long> {
 }
