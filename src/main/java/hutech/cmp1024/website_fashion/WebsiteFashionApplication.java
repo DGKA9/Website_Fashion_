e3e7f8c4cc5cbc56b30717e3d17df0133afcb5b7
Dang Khoa <dangkhoa200299@gmail.com>
2023-05-25 14:17:07 +0700

    main

---
+++ src/main/java/hutech/cmp1024/website_fashion/WebsiteFashionApplication.java
@@ -0,0 +1,13 @@
+package hutech.cmp1024.website_fashion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteFashionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteFashionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hutech/cmp1024/website_fashion/WebsiteFashionApplicationTests.java
@@ -0,0 +1,13 @@
+package hutech.cmp1024.website_fashion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteFashionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
